bea2c2c3d610b2da746c6729a78d175fe1274b47
Abdullah <uytghu600@gmail.com>
2020-09-14 03:21:05 +0300

    Initial Commit

---
+++ iPawStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPawStore
+//
+//  Created by Abdullah on 26/01/1442 AH.
+//  Copyright © 1442 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
